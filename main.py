ee4d312d81f57a1f4c3f50a31c2b4920dec12f20
JunBin <j.junbin@qq.com>
2023-01-18 13:42:39 +0800

    added print factor function

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
+import math
+
 def main():
-	print("Hello World")
+	num = input("Find all factors of : ")
+
+	print_factor(int(num))
+
+def print_factor(x):
+	s = set()
+	mid = math.ceil(math.sqrt(x))
+
+	for i in range(1,mid):
+		if(x%i==0):
+			s.add(i)
+			s.add(int(x/i))
+
+	l = list(s)
+	l.sort()
+	print(l)
+
 if __name__ == '__main__':
 	main()
