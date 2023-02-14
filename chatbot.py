afeb176020d6abba59cd44b2b86e1f8be9c52391
JunBin <j.junbin@qq.com>
2023-02-14 22:19:26 +0800

    lab4: more handlers; added external redis server

--- chatbot.py
+++ chatbot.py
@@ -14,7 +14,6 @@ Basic Echobot example, repeats messages.
 Press Ctrl-C on the command line or send a signal to the process to stop the
 bot.
 """
-JunBin_Bot_TOKEN="6002545689:AAHRYBH3yY_turaUx6wi9DjJU1di00mR8OI"
 
 import logging
 
@@ -31,8 +30,13 @@ if __version_info__ < (20, 0, 0, "alpha", 1):
         f"{TG_VER} version of this example, "
         f"visit https://docs.python-telegram-bot.org/en/v{TG_VER}/examples.html"
     )
+
+import configparser
+import redis
 from telegram import ForceReply, Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+
+global redis1
 
 # Enable logging
 logging.basicConfig(
@@ -54,7 +58,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /help is issued."""
-    await update.message.reply_text("Help!")
+    await update.message.reply_text("Helping you helping you!")
 
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -62,17 +66,49 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(update.message.text)
 
 
+async def add(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /add is issued."""
+    try:
+        global redis1
+        logging.info(context.args[0])
+        msg = context.args[0]  # /add keyword <-- this should store the keyword
+        redis1.incr(msg)
+        await update.message.reply_text(
+            'You have said ' + msg + ' for ' + redis1.get(msg).decode('UTF-8') + ' times.')
+    except (IndexError, ValueError):
+        await update.message.reply_text('Usage: /add <keyword>')
+
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Echo the user message."""
+    await update.message.reply_text("Good day, "+context.args[0]+"!")
+
+
 def main() -> None:
     """Start the bot."""
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
+    global redis1
+    redis1 = redis.Redis(host=(config['REDIS']['HOST']), password=(config['REDIS']
+                                                                   ['PASSWORD']), port=(config['REDIS']['REDISPORT']))
+
+    logging.basicConfig(
+        format='% (asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
     # Create the Application and pass it your bot's token.
-    application = Application.builder().token(JunBin_Bot_TOKEN).build()
+    application = Application.builder().token(
+        config['TELEGRAM']['ACCESS_TOKEN']).build()
 
     # on different commands - answer in Telegram
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
+    application.add_handler(CommandHandler("add", add))
+    application.add_handler(CommandHandler("hello", hello))
 
     # on non command i.e message - echo the message on Telegram
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    application.add_handler(MessageHandler(
+        filters.TEXT & ~filters.COMMAND, echo))
 
     # Run the bot until the user presses Ctrl-C
     application.run_polling()
